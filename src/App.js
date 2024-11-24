b9973e6fb6c4b91b4cb68c1b79a2830bc4552e12
jmiguelsg78 <jmiguelsg78@educastur.es>
2024-11-25 00:55:28 +0100

    100%

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Saludo from './componentes/Saludo';
+import Despedida from './componentes/Despedida';
 
 function App() {
+
+  const usuario = true;
+  const nombre = "Jose Miguel";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    {usuario ? <Saludo nombre={nombre} /> : <Despedida nombre={nombre} />}
+   </div>
   );
 }
 
